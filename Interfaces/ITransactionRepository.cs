1b533d4f9c25e77a6f14933f562513afdefdfd09
wahid shaikh <s.wahid2585@gmail.com>
2024-12-10 16:58:37 +0530

    AddTransaction Method Added In Transaction Repository.

--- Interfaces/ITransactionRepository.cs
+++ Interfaces/ITransactionRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace BankAccountManagement.Interfaces
+﻿using System.Transactions;
+
+namespace BankAccountManagement.Interfaces
 {
     public interface ITransactionRepository
     {
+        int AddTransaction(Transaction transaction);
+
+        Transaction GetTransaction(int transactionId);
     }
 }
--- Repositories/TransactionRepository.cs
+++ Repositories/TransactionRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace BankAccountManagement.Repositories
+﻿using System.Data;
+using BankAccountManagement.Models;
+using BankAccountManagement.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace BankAccountManagement.Repositories
 {
-    public class TransactionRepository
+    public class TransactionRepository : ITransactionRepository
     {
+        private readonly string _connectionString;
+
+        public TransactionRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int AddTransaction(Transaction transaction)
+
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+           
+            {
+                string query = "Insert Into Transactions (TransactionID, AccountID, Amount, TransactionType)" +
+                    "Values (@TransactionID, @AccountID, @Amount, @TransactionType); SLECT SCOPE IDENTITY();";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TransactionID", transaction.TransactionID);
+                command.Parameters.AddWithValue("@AccountID", transaction.AccountID);
+                command.Parameters.AddWithValue("@Amount", transaction.Amount);
+                command.Parameters.AddWithValue("@TransactionType", transaction.TransactionType);
+
+                connection.Open();
+                int newTransactionID = Convert.ToInt32(command.ExecuteNonQuery());
+                return newTransactionID;
+            }
+        }
     }
 }
