c75d1f7a72af1f3171acfe58026cee923e8f0ee4
wahid shaikh <s.wahid2585@gmail.com>
2024-12-10 17:55:43 +0530

    Added Dependancy Injection of Interface in Program.cs file.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using BankAccountManagement.Interfaces;
+using BankAccountManagement.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Register repositories for dependency injection
+builder.Services.AddSingleton<IAccountRepository, AccountRepository>();
+builder.Services.AddSingleton<ITransactionRepository, TransactionRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
