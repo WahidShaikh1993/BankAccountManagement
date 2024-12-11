f5a7a49e9f2dbe207d64a3c58e2ebcc693e5993f
wahid shaikh <s.wahid2585@gmail.com>
2024-12-11 19:39:58 +0530

    Http Post Request Added in AccountController.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankAccountManagement.Interfaces;
+using BankAccountManagement.Models;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace BankAccountManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(IAccountRepository accountRepository)
+
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateAccount([FromBody] Account account)
+
+        {
+            int accountID = _accountRepository.CreateAccount(account);
+            return CreatedAtAction(nameof(CreateAccount), new { id = accountID }, account);
+        }
+
+    }
+}
